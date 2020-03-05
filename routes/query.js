f403dd200d9e55c8d41e4e525896647844645811
furuta <>
2020-03-05 18:47:00 -0500

    add debug code for getting user id

--- routes/query.js
+++ routes/query.js
@@ -39,8 +39,15 @@ router.get('/', async function(req, res, next) {
             const network = await gateway.getNetwork('mychannel');
     
             // Get the contract from the network.
-            const contract = network.getContract('fabcar');
-    
+            const contract = network.getContract('record_dev');
+   
+            // just debug
+            const id = await contract.evaluateTransaction('getUserId');
+            console.log(id);
+
+            const attr = await contract.evaluateTransaction('getUserAttr');
+            console.log(attr);
+
             // Evaluate the specified transaction.
             // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
             // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
