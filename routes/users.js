b283663591ab8a9ae478832394a9d60ad5edd4a3
furuta <>
2020-03-08 20:36:34 -0400

    modify register function for already existing

--- routes/users.js
+++ routes/users.js
@@ -56,7 +56,7 @@ router.get('/', async function(req, res, next) {
           // Evaluate the specified transaction.
           // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
           // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
-          const result = await contract.evaluateTransaction('createPatientRecord', name);
+          const result = await contract.submitTransaction('createPatientRecord', name);
           console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
 
           // if (result) {
